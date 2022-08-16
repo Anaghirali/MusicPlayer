a36176879f2588998aba0b7ee69ef0ee98cecd8d
Ana <anaghirali@gmail.com>
2022-08-16 11:31:18 -0300

    first commit

---
+++ base/data.js
@@ -0,0 +1,32 @@
+export default [
+    { 
+      title: "La Vie En Rose",
+      artist: "Music",
+      cover: "sadcat.jpg",
+      file: "lavieenrose.mp3"
+    },
+    {
+     title:"BadGuy",
+      artist: "BillieEilish",
+      cover: "billiedog.jpg",
+      file: "BadGuy.mp3"
+    },
+    {
+      title: "Pass The Dutchie",
+      artist: "Musical Youth",
+      cover: "mousemarley.jpg",
+      file: "PassTheDutchie.mp3"
+    },
+    {
+      title: "Old Town Road",
+      artist: "Lil Nas X",
+      cover: "pugcowboy.jpg",
+      file: "OldTownRoad.mp3"
+    },
+    {
+      title: "P.I.M.P",
+      artist: "50 Cent",
+      cover: "tuglifecat.jpg",
+      file: "P.I.M.P.mp3"
+    }
+  ];                                                                   
